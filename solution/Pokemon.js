245a02ae64030e486d7f6ec5b5f312a207d8e328
oliverjam <oliverjamesphillips@gmail.com>
2019-05-25 18:14:36 +0100

    Remove comment from Pokemon solution

--- solution/Pokemon.js
+++ solution/Pokemon.js
@@ -22,36 +22,5 @@ function Pokemon({ name }) {
     </div>
   );
 }
-// class Pokemon extends React.Component {
-//   state = {
-//     loading: true,
-//     data: null,
-//   };
-//   componentDidMount() {
-//     const { name } = this.props;
-//     getPokemon(name).then(data => this.setState({ data, loading: false }));
-//   }
-//   componentDidUpdate(prevProps) {
-//     const { name } = this.props;
-//     // re-fetch new pokemon if name has changed
-//     if (prevProps.name !== name) {
-//       this.setState({ loading: true });
-//       getPokemon(name).then(data => this.setState({ data, loading: false }));
-//     }
-//   }
-//   render() {
-//     const { loading, data } = this.state;
-//     if (loading) return <div>Loading...</div>;
-//     return (
-//       <div>
-//         <h2>{this.props.name}</h2>
-//         <img
-//           src={data.sprites.front_default}
-//           alt={`${data.name} default sprite`}
-//         />
-//       </div>
-//     );
-//   }
-// }
 
 export default Pokemon;
